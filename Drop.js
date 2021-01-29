278f6a799fc190752b57f975ef65b866c13771a0
renanll <renan_ll@hotmail.com>
2021-01-29 16:49:05 -0300

    first commit

---
+++ Drop.js
@@ -0,0 +1,19 @@
+const { STROKE } = require("./p5");
+const x , y, yspeed;
+class Drop{
+    constructor(){
+        this.x = width/2;
+        this.y = 0;
+        this.yspeed = 1;
+    
+
+        function falls() {
+            y = y +yspeed;
+        }
+
+        function show() {
+            STROKE(138,43,226);
+            line(x,y,x,y+10);
+        }
+    }
+}
