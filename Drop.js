616aaf775a48e7e422a8b2e3dfaa3c2b798a3abf
renanll <renan_ll@hotmail.com>
2022-01-12 17:56:20 -0300

    CHORES:Arrumando estrela

--- Drop.js
+++ Drop.js
@@ -1,19 +1,20 @@
 const { STROKE } = require("./p5");
-const x , y, yspeed;
-class Drop{
-    constructor(){
-        this.x = width/2;
+const x, y, yspeed;
+class Drop {
+    constructor() {
+        this.x = width / 2;
         this.y = 0;
         this.yspeed = 1;
-    
+
 
         function falls() {
-            y = y +yspeed;
+            y = y + yspeed;
         }
 
         function show() {
-            STROKE(138,43,226);
-            line(x,y,x,y+10);
+            STROKE(138, 43, 226);
+            line(x, y, x, y + 10);
+            line(y, x, y, x + 10)
         }
     }
 }
