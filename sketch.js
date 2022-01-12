310e9ad7600968b387b3a9fd3f80b912f38331d9
renanll <renan_ll@hotmail.com>
2022-01-12 16:59:18 -0300

    CHORES:Quase pronto falta dazer um temp

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+
+var clock = 0
+
 class Drop {
   constructor() {
     this.x = random(200, -200);
@@ -6,13 +9,14 @@ class Drop {
     this.len = map(this.z, 0, 20, 10, 20);
     this.yspeed = map(this.z, 0, 20, 0, 1);
     this.xspeed = map(this.z, 0, 10, 0, 20);
+
   }
 
   fall() {
     this.y = this.y + this.yspeed;
     this.x = this.x + this.yspeed;
     var grav = map(this.z, 0, 20, 0, 0.2);
-    this.yspeed = this.yspeed + grav;
+    this.yspeed = this.yspeed + 0.5 * grav;
     if (this.y > height) {
       this.y = random(-100, -50);
       this.yspeed = map(this.z, 0, 20, 2, 0);
@@ -24,14 +28,16 @@ class Drop {
     strokeWeight(thick);
     stroke(255, 255, 255);
     line(this.x, this.y, this.x + this.len, this.y + this.len + this.len + this.xspeed);
+    line(this.y, this.x, this.y + this.len, this.x + this.len + this.len + this.yspeed);
   }
 }
 
 var drops = [];
 
+
 function setup() {
   createCanvas(1000, 750);
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 5; i++) {
     drops[i] = new Drop();
   }
 }
@@ -43,3 +49,4 @@ function draw() {
     drops[i].show();
   }
 }
+
